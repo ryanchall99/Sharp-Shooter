c7393fbcdb609c79069874664cf2b7d44d652b2b
ryanchall99 <rileychall@hotmail.co.uk>
2025-01-16 14:35:21 +0000

    Simple Screen Shake
    - Added a Cinemachine impulse listener to the player follow camera.
    - Added a Cinemachine impulse source to each weapon. (Affects the Y Axis)
    - When a weapon is fired, the attached impulse source is generated.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -8,9 +9,17 @@ public class Weapon : MonoBehaviour
     [Header("Interactions")]
     [SerializeField] LayerMask interactionLayers;
 
+    CinemachineImpulseSource impulseSource;
+
+    void Awake() 
+    {
+        impulseSource = GetComponent<CinemachineImpulseSource>();    
+    }
+
     public void Shoot(WeaponSO weaponSO)
     {
         muzzleFlash.Play();
+        impulseSource.GenerateImpulse();
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, interactionLayers, QueryTriggerInteraction.Ignore))
